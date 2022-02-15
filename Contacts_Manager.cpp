86035fffa7b241be734c50196e6b8ae71650c745
imanipourmeysam <imanipourmeysam@gmail.com>
2022-02-15 10:07:55 +0100

    Editing the Contacts_Manager.h and Contacts_Manager.cpp

--- Contacts_Manager.cpp
+++ Contacts_Manager.cpp
@@ -12,6 +12,74 @@ Contacts_Manager::Contacts_Manager(strp _Dir)
     Dir = _Dir;
 }
 
+char Contacts_Manager::whatdata(strp _Data)
+{
+    
+    if(_Data.find('@')!= std::string::npos||_Data == "not defined")
+    {
+        return 'e';
+    }
+    else if (std::isdigit(_Data[0]) == 1)
+    {
+        return 'p';
+    }
+    else
+    {
+         return 'n'; 
+    }
+}
+
+int Contacts_Manager::find(strp _Dataitem)
+{
+    char datatype = this->whatdata(_Dataitem);
+    int fline = 0;
+    Fo.close();
+    Fi.open(Dir);
+    std::string line;
+    while(std::getline(Fi,line))
+    {
+        size_t firstcomma = line.find(',',0);
+        size_t secondcomma= line.find(',',firstcomma+1);
+        size_t res = line.find(_Dataitem);
+        if(res != std::string::npos)
+        {
+            switch(datatype)
+            {
+                case 'n':
+                    {
+                        if(!line.compare(0,firstcomma,_Dataitem))
+                        {
+                             Fi.close();
+                             return fline;
+                        }
+                    }break;
+                case 'p':
+                    {
+                        if(!line.compare(firstcomma+1,secondcomma-firstcomma-1,_Dataitem))
+                        {
+                             Fi.close();
+                             return fline; 
+                        }
+
+                    }break;
+                case 'e':
+                    {
+                        if(!line.compare(secondcomma+1,line.length()-secondcomma-1,_Dataitem))
+                        {
+                             Fi.close();
+                             return fline;                            
+                        }
+                    }break;
+            }
+
+        }
+        fline++;
+    }
+    fline = -1;
+    Fi.close();
+    return fline;
+}
+
 void Contacts_Manager::newcontact(const NPE& _NPE)
 {
     Fi.close();
@@ -109,74 +177,6 @@ void Contacts_Manager::removecontact(strp _CurrentData)
     Fo.close();
 }
 
-int Contacts_Manager::find(strp _Dataitem)
-{
-    char datatype = this->whatdata(_Dataitem);
-    int fline = 0;
-    Fo.close();
-    Fi.open(Dir);
-    std::string line;
-    while(std::getline(Fi,line))
-    {
-        size_t firstcomma = line.find(',',0);
-        size_t secondcomma= line.find(',',firstcomma+1);
-        size_t res = line.find(_Dataitem);
-        if(res != std::string::npos)
-        {
-            switch(datatype)
-            {
-                case 'n':
-                    {
-                        if(!line.compare(0,firstcomma,_Dataitem))
-                        {
-                             Fi.close();
-                             return fline;
-                        }
-                    }break;
-                case 'p':
-                    {
-                        if(!line.compare(firstcomma+1,secondcomma-firstcomma-1,_Dataitem))
-                        {
-                             Fi.close();
-                             return fline; 
-                        }
-
-                    }break;
-                case 'e':
-                    {
-                        if(!line.compare(secondcomma+1,line.length()-secondcomma-1,_Dataitem))
-                        {
-                             Fi.close();
-                             return fline;                            
-                        }
-                    }break;
-            }
-
-        }
-        fline++;
-    }
-    fline = -1;
-    Fi.close();
-    return fline;
-}
-
-char Contacts_Manager::whatdata(strp _Data)
-{
-    
-    if(_Data.find('@')!= std::string::npos||_Data == "not defined")
-    {
-        return 'e';
-    }
-    else if (std::isdigit(_Data[0]) == 1)
-    {
-        return 'p';
-    }
-    else
-    {
-         return 'n'; 
-    }
-}
-
 void Contacts_Manager::edit(strp _CurrentName, strp _NewData)
 {
     int fline = this->find(_CurrentName);
@@ -229,4 +229,130 @@ void Contacts_Manager::edit(strp _CurrentName, strp _NewData)
     Fo.close();
     return;
 }
+//I was thinking if I could use multiprocessing then I would invok two times edit function with _NewData 1 and 2.
+void Contacts_Manager::edit(strp _CurrentData, strp _NewData1, strp _NewData2)
+{
+    if(this->whatdata(_NewData1) == this->whatdata(_NewData2))
+    {
+       std::cout << "Please enter two different types of data.\n";
+       std::cout << "More detial -> " << _NewData1 << " and " << _NewData2 << "are both the same type.\n";
+       return;
+    }
+    if(this->whatdata(_NewData1) != this->whatdata(_CurrentData))
+    {
+        this->edit(_CurrentData,_NewData1);
+        this->edit(_CurrentData,_NewData2);
+        return;
+    }
+    else if(this->whatdata(_NewData2) != this->whatdata(_CurrentData))
+    {
+        this->edit(_CurrentData,_NewData2);
+        this->edit(_CurrentData,_NewData1);
+        return;
+    }
+    else 
+    {
+        this->edit(_CurrentData,_NewData2);
+        this->edit(_CurrentData,_NewData1);
+        return;
+    }
+}
+
+void Contacts_Manager::edit(strp _CurrentData, strp _NewData1, strp _NewData2, strp _NewData3)
+{
+    char input_types[3] = {this->whatdata(_NewData1),this->whatdata(_NewData2),this->whatdata(_NewData3)};
+    if(input_types[0] == input_types[1] || input_types[0] == input_types[2] || input_types[1] == input_types[2])
+    {
+       std::cout << "Please enter different types of new data.\n";
+       return;
+    }
+
+    if(input_types[0] != this->whatdata(_CurrentData))
+    {
+        this->edit(_CurrentData,_NewData1);
+        if(input_types[1] != this->whatdata(_CurrentData))
+        {
+            this->edit(_CurrentData,_NewData2);
+            this->edit(_CurrentData,_NewData3);
+            return;
+        }
+        else if(input_types[2] != this->whatdata(_CurrentData))
+        {
+            this->edit(_CurrentData,_NewData3);
+            this->edit(_CurrentData,_NewData2);
+            return;
+        }
+    }
+
+    if(input_types[1] != this->whatdata(_CurrentData))
+    {
+        this->edit(_CurrentData,_NewData2);
+        if(input_types[0] != this->whatdata(_CurrentData))
+        {
+            this->edit(_CurrentData,_NewData1);
+            this->edit(_CurrentData,_NewData3);
+            return;
+        }
+        else if(input_types[2] != this->whatdata(_CurrentData))
+        {
+            this->edit(_CurrentData,_NewData3);
+            this->edit(_CurrentData,_NewData1);
+            return;
+        }
+    }
+    
+    if(input_types[2] != this->whatdata(_CurrentData))
+    {
+        this->edit(_CurrentData,_NewData3);
+        if(input_types[0] != this->whatdata(_CurrentData))
+        {
+            this->edit(_CurrentData,_NewData1);
+            this->edit(_CurrentData,_NewData2);
+            return;
+        }
+        else if(input_types[1] != this->whatdata(_CurrentData))
+        {
+            this->edit(_CurrentData,_NewData2);
+            this->edit(_CurrentData,_NewData1);
+            return;
+        }
+    }
+
+}
+
+
+void Contacts_Manager::printinfo(strp _SearchData)
+{
+    int fline = this->find(_SearchData);
+    if(fline == -1)
+    {
+        std::cout << "No contact has been found" << "\n";
+        return;
+    }
+    Fo.close();
+    Fi.open(Dir);
+    int cnt = 0;
+    std::string line;
+    while(std::getline(Fi,line))
+    {
+        if(cnt == fline)
+        {
+            size_t firstcomma = line.find(',',0);
+            size_t secondcomma= line.find(',',firstcomma+1);
+            std::cout << "Name  : " << line.substr(0,firstcomma) << "\n";
+            std::cout << "Phone : " << line.substr(firstcomma+1,secondcomma-firstcomma-1) << "\n";
+            std::cout << "Email : " << line.substr(secondcomma+1,line.length()-secondcomma-1) << "\n";
+            return;
+        }
+        cnt++;
+    }
+}
+
+void Contacts_Manager::removeall()
+{
+    Fi.close();
+    Fo.open(Dir,std::ofstream::trunc);
+    return;
+}
+
 
--- Contacts_Manager.h
+++ Contacts_Manager.h
@@ -20,16 +20,20 @@ public:
     Contacts_Manager(strp _Dir);
     void newcontact(const NPE& _NPE);
     void newcontact(strp _Name, strp _Phone, strp _Email = "not defined");
-    void removecontact(strp _CurrentData);//remove from phonebook(where we store contacts) the contact include _CurrentData
-    void edit(strp _CurrentData, strp _NewData);//edit the info of a contact which include _CurrentData with replacing _NewData.
+    void removecontact(strp _CurrentData);//remove the contact which include _CurrentData
+    void edit(strp _CurrentData, strp _NewData);//edit info of a contact which include _CurrentData with replacing _NewData(s).
+    void edit(strp _CurrentData, strp _NewData1, strp _NewData2);
+    void edit(strp _CurrentData, strp _NewData1, strp _NewData2, strp _NewData3);
+    void printinfo(strp _SearchData);//it fill find a contact based _SearchData and print its info.
+    void removeall();
 private:
     std::string Dir;// path to phonebook, where we store contacts info.
     std::ofstream Fo;
     std::ifstream Fi;
-    int find(strp _Dataitem);//return the number of the line containing _Dataitem.(_Dataitem could be name, phone or email).
     std::set<std::string> tempo_phonebook;
     char whatdata(strp _Data);//return 'n' if _Data is name, return 'p' if _Data is Phone and 'e' if _Data is email. 
-
+    int find(strp _Dataitem);//return the number of the line containing _Dataitem.(_Dataitem could be name, phone or email).
+    
 };
 
 #endif
